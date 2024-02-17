1ec74ad0f251f538a90aea67244d161ad84174f0
addmy01 <70408689+addmy01@users.noreply.github.com>
2024-02-17 16:42:41 +0000

    final change

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronouns = ["the", "our"];
+  let adjectives = ["great", "big"];
+  let nouns = ["jogger", "racoon"];
+  let extensions = ["org", "net", "com"];
+  for (let pronoun of pronouns) {
+    for (let adjective of adjectives) {
+      for (let noun of nouns) {
+        for (let extension of extensions) {
+          let div = document.createElement(`div`);
+          div.innerText = `${pronoun}${adjective}${noun}.${extension}`;
+
+          // Remove trailing dot if it exists
+          // if (domain.endsWith(".")) {
+          //   domain = domain.slice(0, -1);
+          // }
+          document.body.appendChild(div);
+          // console.log(domain);
+        }
+      }
+    }
+  }
 };
